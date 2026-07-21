79878ee78eacf8430c85e3da0ecd5776c9c82f45
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2026-07-21 23:54:18 +0500

    Day2: scaffolding, assets, icon

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Center(
-      child: Text("Hello World"),
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey,
+          title: Text("Hello Flutter"),
+        ),
+        body: const Center(child: Image(image: AssetImage('assets/images/green.jpg'))),
+      ),
     ),
-  ));
+  );
 }
