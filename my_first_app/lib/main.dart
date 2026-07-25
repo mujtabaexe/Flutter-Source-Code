fc450a8a7d604c167d7ab19a8c5c9003c2ac97c9
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2026-07-25 13:49:43 +0500

    Day4: fonts,style,decoration

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -11,16 +11,60 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.pink,
         appBar: AppBar(
           backgroundColor: Colors.teal,
-          title: const Text("Hello Flutter"),
+          title: Center(
+            child: const Text(
+              "Hello Flutter",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 30,
+                fontFamily: 'Pacifico',
+                fontWeight: FontWeight.w100,
+              ),
+            ),
+          ),
         ),
-        body: Column(
-          children: const [
-            Center(child: Text('First Widget')),
-            Text('Second Widget'),
-          ],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Hello World 1',
+                style: TextStyle(
+                  fontSize: 40,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.teal,
+                  fontFamily: 'Pacifico',
+                  color: Colors.teal,
+                ),
+              ),
+
+              SizedBox(height: 40),
+
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.pink,
+                child: const Center(child: Text('Container 1')),
+              ),
+              const SizedBox(height: 15, width: 15),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.blue,
+                child: const Center(child: Text('Container 2')),
+              ),
+              const SizedBox(height: 15, width: 15),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.teal,
+                child: const Center(child: Text('Container 3')),
+              ),
+            ],
+          ),
         ),
       ),
     );
