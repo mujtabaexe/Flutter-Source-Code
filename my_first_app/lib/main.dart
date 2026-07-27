e9e1fcf9a00c9a4e482099b35d1862bc120d803b
samujtaba99 <sahibzadaahmadmujtaba@gmail.com>
2026-07-27 22:16:18 +0500

    Designing UI

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -64,6 +64,44 @@ class MyApp extends StatelessWidget {
                 child: const Center(child: Text('Container 3')),
               ),
             ],
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Hello World 1',
+                style: TextStyle(
+                  fontSize: 40,
+                  decoration: TextDecoration.underline,
+                  decorationColor: Colors.teal,
+                  fontFamily: 'Pacifico',
+                  color: Colors.teal,
+                ),
+              ),
+
+              SizedBox(height: 40),
+
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.pink,
+                child: const Center(child: Text('Container 1')),
+              ),
+              const SizedBox(height: 15, width: 15),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.blue,
+                child: const Center(child: Text('Container 2')),
+              ),
+              const SizedBox(height: 15, width: 15),
+              Container(
+                height: 100,
+                width: 100,
+                color: Colors.teal,
+                child: const Center(child: Text('Container 3')),
+              ),
+            ],
           ),
         ),
       ),
